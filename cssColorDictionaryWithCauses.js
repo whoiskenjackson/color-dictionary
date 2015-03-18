200e8616d96b5e8949773f5f013e62bec154d285
Kenneth Jackson <kenneth@whoiskenjackson.com>
2015-03-18 15:34:56 -0700

    Update cssColorDictionaryWithCauses.js

--- cssColorDictionaryWithCauses.js
+++ cssColorDictionaryWithCauses.js
@@ -88,7 +88,69 @@ var cssColorDictionaryWithCauses = [
     "name": "blue",
     "hex": "0000ff",
     "rgb": "rgb(0, 0, 255)",
-    "causes": []
+    "causes": [
+      "Alopecia",
+      "Ankylosing Spondylitis",
+      "Acute Respiratory Distress Syndrome (ARDS)",
+      "Anti-Bullying",
+      "Brachial Plexus Injuries",
+      "Child Abuse",
+      "Children&#39;s Healthy Weight",
+      "Colitis",
+      "Colon Cancer",
+      "Crime Victim&#39;s Rights",
+      "Crohn&#39;s Disease ",
+      "Cru du Chat Syndrome",
+      "Drowning Awareness",
+      "Dysautonomia",
+      "Dystonia",
+      "Education",
+      "Epstein-Barr Virus",
+      "Erb&#39;s Palsy",
+      "Familial Polyposis",
+      "Free Speech",
+      "Foster Care Awareness",
+      "Guillain Barre Syndrome",
+      "Hydrocephalus (US)",
+      "Huntington&#39;s Disease",
+      "Hurricanes",
+      "Hystiocytosis",
+      "Ichthyosis",
+      "Interstitial Cystitis",
+      "Langerhans Cell Hystiocytosis",
+      "Leukodystrophies",
+      "ME/CFIDS",
+      "Myositis",
+      "Prevent School Violence",
+      "Osteogenesis Imperfecta (IO)",
+      "Pompe Disease",
+      "Restless Legs Syndrome",
+      "Steven Johnson Syndrome",
+      "Teens against Smoking",
+      "Victim&#39;s Rights",
+      "Victims of Hurricane Katrina",
+      "Save the Music",
+      "Water accidents",
+      "Water Quality and Water Safety",
+      "Arthritis",
+      "Chronic Fatigue Syndrome",
+      "Colorectal Cancer",
+      "Drunk Driving",
+      "Police Lost in Line of Duty",
+      "Transverse Myelitis",
+      "Designated Drivers",
+      "Down Syndrome",
+      "Thoracic Outlet Syndrome",
+      "Pediatric Stroke",
+      "Rheumatoid Arthritis",
+      "Congenital Heart Defects and Disease",
+      "Hypo plastic left heart Syndrome",
+      "Olser Weber Rendu Syndrome (HHT)",
+      "Noonan&#39;s Syndrome",
+      "Pulmonary Fibrosis",
+      "Rare Lung Diseases",
+      "SADS"
+    ]
   },
   {
     "name": "blueviolet",
@@ -100,7 +162,13 @@ var cssColorDictionaryWithCauses = [
     "name": "brown",
     "hex": "a52a2a",
     "rgb": "rgb(165, 42, 42)",
-    "causes": []
+    "causes": [
+      "Anti-Tobacco",
+      "Colon Cancer",
+      "Colorectal Cancer",
+      "Familia Polyposis",
+      "Tobacco Awareness"
+    ]
   },
   {
     "name": "burlywood",
@@ -220,7 +288,32 @@ var cssColorDictionaryWithCauses = [
     "name": "darkred",
     "hex": "8b0000",
     "rgb": "rgb(139, 0, 0)",
-    "causes": []
+    "causes": [
+      "Adhesions",
+      "AV Malformation",
+      "Brain Aneurysm",
+      "Cesarean Section",
+      "Cystic Hygroma",
+      "Disabled Adults",
+      "Factor V Leiden",
+      "Headaches",
+      "Hereditary Hemochromatosis",
+      "Hospice Care",
+      "Lymphatic Malformation",
+      "Meningitis",
+      "Meningococcal Meningitis",
+      "PHACES",
+      "Post-Polio Syndrome",
+      "Sickle Cell Anemia",
+      "Sturge-Weber Syndrome",
+      "Thrombophilia",
+      "Williams Syndrome",
+      "Antiphospholipid Antibody Syndrome",
+      "Hemangioma",
+      "Vascular Malformation",
+      "Hughes Syndrome",
+      "Multiple Myeloma"
+    ]
   },
   {
     "name": "darksalmon",
@@ -322,7 +415,14 @@ var cssColorDictionaryWithCauses = [
     "name": "gold",
     "hex": "ffd700",
     "rgb": "rgb(255, 215, 0)",
-    "causes": []
+    "causes": [
+      "Alveolar Rhabdomyosarcoma",
+      "Childhood Cancer",
+      "COPD",
+      "Embryonal Rhabdomyosarcoma",
+      null,
+      "Osteosarcoma"
+    ]
   },
   {
     "name": "goldenrod",
@@ -436,7 +536,16 @@ var cssColorDictionaryWithCauses = [
     "name": "lavender",
     "hex": "e6e6fa",
     "rgb": "rgb(230, 230, 250)",
-    "causes": []
+    "causes": [
+      "Cancer (unspecified)",
+      "Caregiver Appreciation",
+      "Epilepsy (alt purple)",
+      "Foster Care",
+      "Hypokalemic Periodic Paralysis",
+      "Infantile Spasms",
+      "Gynecological Cancer",
+      "Rett Syndrome"
+    ]
   },
   {
     "name": "lavenderblush",
@@ -460,7 +569,26 @@ var cssColorDictionaryWithCauses = [
     "name": "lightblue",
     "hex": "add8e6",
     "rgb": "rgb(173, 216, 230)",
-    "causes": []
+    "causes": [
+      "Addisons Disease",
+      "Chronic Illness",
+      "Cushing Syndrome",
+      "DiGeorge Sequence",
+      "Graves Disease",
+      "Hydrocephalus",
+      "Klinefelter&#39;s Syndrome",
+      "Lymphedema",
+      "Pro Choice",
+      "Prostate Cancer",
+      "Scleroderma",
+      "Shprintzen Syndrome",
+      "Thyroid Disease",
+      "Trisomy 18 and Velo-Cardio Facial Syndrome",
+      "Behcets Disease",
+      "Hyperaldosteronism",
+      "Men&#39;s Health",
+      "Spay or Neuter Pets"
+    ]
   },
   {
     "name": "lightcoral",
@@ -490,7 +618,11 @@ var cssColorDictionaryWithCauses = [
     "name": "lightgreen",
     "hex": "90ee90",
     "rgb": "rgb(144, 238, 144)",
-    "causes": []
+    "causes": [
+      "Celiac Disease, Chronic pelvic Pain",
+      "Human Papilloma Virus",
+      "Sexually Transmitted Diseases (STD)"
+    ]
   },
   {
     "name": "lightpink",
@@ -544,7 +676,15 @@ var cssColorDictionaryWithCauses = [
     "name": "limegreen",
     "hex": "32cd32",
     "rgb": "rgb(50, 205, 50)",
-    "causes": []
+    "causes": [
+      "Duchenne Muscular Dystrophy",
+      "Gastroschisis",
+      "Lyme Disease",
+      "Lymphoma",
+      "Muscular Dystrophy",
+      "Non-Hodgkin&#39;s Lymphoma",
+      "Sandhoff Disease"
+    ]
   },
   {
     "name": "linen",
@@ -586,7 +726,20 @@ var cssColorDictionaryWithCauses = [
     "name": "mediumpurple",
     "hex": "9370db",
     "rgb": "rgb(147, 112, 219)",
-    "causes": []
+    "causes": [
+      "Acid Reflux",
+      "Bulimia Nervosa",
+      "Eating Disorders",
+      "Eosinophilic Disorders",
+      "Gastric Cancer",
+      "Gastroesophageal Reflux Disease(GERD)",
+      "Pulmonary Hypertension",
+      "Small Intestine Cancer",
+      "Anorexia Nervosa",
+      "Esophageal Cancer",
+      "Irritable Bowel Syndrome(IBS)",
+      "Stomach Cancer"
+    ]
   },
   {
     "name": "mediumseagreen",
@@ -702,7 +855,21 @@ var cssColorDictionaryWithCauses = [
     "name": "orange",
     "hex": "ffa500",
     "rgb": "rgb(255, 165, 0)",
-    "causes": []
+    "causes": [
+      "Agent Orange",
+      "Cultural Diversity",
+      "Hunger",
+      "Leukemia",
+      "Lupus",
+      "Melanoma",
+      "Kidney Cancer (Renal Cell Carcinoma)",
+      "Motorcycle Safety",
+      "Multiple sclerosis (MS)",
+      "Reflex Sympathetic Dystrophy Syndrome",
+      "Self Injury",
+      "Racial Tolerance",
+      "Prader-Willi Syndrome"
+    ]
   },
   {
     "name": "orangered",
@@ -714,7 +881,10 @@ var cssColorDictionaryWithCauses = [
     "name": "orchid",
     "hex": "da70d6",
     "rgb": "rgb(218, 112, 214)",
-    "causes": []
+    "causes": [
+      "Eczema",
+      "Psoriasis"
+    ]
   },
   {
     "name": "palegoldenrod",
@@ -762,7 +932,28 @@ var cssColorDictionaryWithCauses = [
     "name": "pink",
     "hex": "ffc0cb",
     "rgb": "rgb(255, 192, 203)",
-    "causes": []
+    "causes": [
+      "Breast Cancer",
+      "Birth Parents",
+      "Nursing Mothers",
+      "Amniotic Fluid Embolism",
+      "Baby Safe Haven",
+      "Birth defects",
+      "Male Breast Cancer",
+      "Hyperemesis Graviduram",
+      "Hyperammonemia",
+      "Infant Loss",
+      "Infertility",
+      "Miscarriage",
+      "Pregnancy Loss",
+      "Premature Birth",
+      "Stillbirth",
+      "Sudden Infant Death Syndrome (SIDS)",
+      "Turners Syndrome",
+      "chILD (Pediatric bronchiolitis obliterans).",
+      "Twin to Twin transfusion syndrome",
+      "Thyroid Cancer Ribbon"
+    ]
   },
   {
     "name": "plum",
@@ -810,14 +1001,9 @@ var cssColorDictionaryWithCauses = [
       "Fibromyalgia",
       "Leimyosarcoma",
       "Victims of 9/11",
-      "Pediatric Stroke",
-      "Rheumatoid Arthritis",
-      "Domestic Violence",
-      "Sexual Assault",
       "Autoimmune Hepatitis",
       "Chemical Injury",
-      "the relationship between Lupus and Endometriosis.",
-      "Thyroid Cancer Ribbon"
+      "the relationship between Lupus and Endometriosis."
     ]
   },
   {
@@ -830,7 +1016,53 @@ var cssColorDictionaryWithCauses = [
     "name": "red",
     "hex": "ff0000",
     "rgb": "rgb(255, 0, 0)",
-    "causes": []
+    "causes": [
+      "AIDS/HIV",
+      "Aperts Syndrome",
+      "Bronchiolitis Obliterans (BOS)",
+      "Burn Victims",
+      "Cardiovascular Disease",
+      "Congenital Heart Disease",
+      "Congestive Heart Failure",
+      "Courage and Inspiration",
+      "DARE",
+      "Driving under the Influence",
+      "Epidermolysis Bellosa",
+      "Factor XI Deficiency",
+      "Heart Disease",
+      "Hemophilia",
+      "High Blood Pressure",
+      "Huffing",
+      "Hypertension",
+      "Inhalant Abuse",
+      "Long Q-T Syndrome",
+      "Wolf-Parkinson-White",
+      "MADD",
+      "Marfan Syndrome",
+      "Project Red Flag",
+      "Reflex Sympathetic Dystrophy",
+      "Sniffing Abuse",
+      "Stroke",
+      "Substance Abuse",
+      "Thrombotic Thrombocytopenic Purpura",
+      "Von Willebrand&#39;s Disease",
+      "Wegener&#39;s Granulomatosis",
+      "Congenital Heart Defects",
+      "Diamond Blackfan Anemia",
+      "Evans Syndrome",
+      "Lymphoma",
+      "Supraventricular Tachycardia",
+      "Tuberculosis",
+      "Aplastic Anemia",
+      "Head and Neck Cancer",
+      "Hereditary Hemorrhagic Telangiectasia",
+      "Oral Cancer",
+      "DVT",
+      "Squamous Cell Carcinoma",
+      "Hepatitis C",
+      "HIV/HCV Co-Infection",
+      "Surviving family members of Suicide victims"
+    ]
   },
   {
     "name": "rosybrown",
@@ -884,7 +1116,22 @@ var cssColorDictionaryWithCauses = [
     "name": "silver",
     "hex": "c0c0c0",
     "rgb": "rgb(192, 192, 192)",
-    "causes": []
+    "causes": [
+      "Brain Disabilities",
+      "Campaign for the Brain",
+      "Charcot-Marie-Tooth (CMT)",
+      "Disabled Children",
+      "Dyslexia",
+      "Elderly Abuse",
+      "Encephalitis",
+      "Niemann-Pick",
+      "Parkinson&#39;s Disease",
+      "Schizophrenia",
+      "Sciatic Pain",
+      "VACTERL",
+      "Brain Disorders",
+      "Young Onset Parkinson&#39;s Association (YOPA)"
+    ]
   },
   {
     "name": "skyblue",
@@ -960,8 +1207,7 @@ var cssColorDictionaryWithCauses = [
       "Uterine Cancer",
       "Domestic Violence",
       "Sexual Assault",
-      "Cervical Cancer",
-      "Thyroid Cancer Ribbon"
+      "Cervical Cancer"
     ]
   },
   {
@@ -986,7 +1232,10 @@ var cssColorDictionaryWithCauses = [
     "name": "violet",
     "hex": "ee82ee",
     "rgb": "rgb(238, 130, 238)",
-    "causes": []
+    "causes": [
+      "Hodgkin&#39;s Disease",
+      "Testicular Cancer"
+    ]
   },
   {
     "name": "wheat",
@@ -998,7 +1247,40 @@ var cssColorDictionaryWithCauses = [
     "name": "white",
     "hex": "ffffff",
     "rgb": "rgb(255, 255, 255)",
-    "causes": []
+    "causes": [
+      "Adoptee",
+      "Adoption",
+      "Anti-Pornography",
+      "Anti-War",
+      "Blindness",
+      "Bone Cancer",
+      "Child exploitation",
+      "Child Sexual Abuse/Assault",
+      "Community Support",
+      "Congenital Cataracts",
+      "Elderly Affairs",
+      "Hernia",
+      "Holocaust Remembrance",
+      "Innocence",
+      "Multiple Hereditary Exostoses",
+      "Molestation Survivor",
+      "On-line Sexual Predator Awareness & Internet Safety",
+      "Peace",
+      "Postpartum Depression",
+      "Retinoblastoma",
+      "Right to Life",
+      "Safe Motherhood",
+      "Salvation Army",
+      "Scoliosis",
+      "Stalking",
+      "Student Sexual Abuse",
+      "Teen Pregnancy Prevention",
+      "Anti-Child Pornography",
+      "Bone Disease",
+      "Osteoporosis",
+      "Victims of Terrorism",
+      "Hope & Support"
+    ]
   },
   {
     "name": "whitesmoke",
@@ -1010,7 +1292,32 @@ var cssColorDictionaryWithCauses = [
     "name": "yellow",
     "hex": "ffff00",
     "rgb": "rgb(255, 255, 0)",
-    "causes": []
+    "causes": [
+      "Adenosarcoma",
+      "Amber Alert",
+      "Bladder Cancer",
+      "Canadian Cancer Society",
+      "Carbon Monoxide Poisoning",
+      "Complementary / Alternative Medicine",
+      "Endometriosis",
+      "Ewings Sarcoma",
+      "Gulf War",
+      "Hydrocephalus (Int&#39;l)",
+      "Liver Cancer",
+      "Liver Disease",
+      "Missing Children",
+      "Myxoid Liposarcoma",
+      "Osteosarcoma",
+      "Obesity",
+      "POW/MIA",
+      "Remembrance",
+      "Sarcoma",
+      "Spina Bifida",
+      "Adoptive Parents",
+      "Equality",
+      "Suicide Awareness",
+      "Troop or Military Support"
+    ]
   },
   {
     "name": "yellowgreen",
